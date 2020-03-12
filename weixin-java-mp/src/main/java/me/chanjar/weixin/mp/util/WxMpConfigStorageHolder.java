07a1d26622baf25e974947f784b74a345cdf0980
Binary Wang <binarywang@gmail.com>
2020-03-12 18:54:52 +0800

    :art: #1430 WxMpConfigStorageHolder提供remove方法，方便使用者调用

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/WxMpConfigStorageHolder.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/WxMpConfigStorageHolder.java
@@ -1,13 +1,11 @@
 package me.chanjar.weixin.mp.util;
 
-
 /**
- * @Author: yd
- * @Date: 2019-03-20 22:06
+ * @author yd
+ * @date 2019-03-20 22:06
  */
 public class WxMpConfigStorageHolder {
-
-  private final static ThreadLocal<String> WX_MP_CONFIG_STORAGE_CHOSE = new ThreadLocal<String>() {
+  private final static ThreadLocal<String> THREAD_LOCAL = new ThreadLocal<String>() {
     @Override
     protected String initialValue() {
       return "default";
@@ -15,11 +13,17 @@ public class WxMpConfigStorageHolder {
   };
 
   public static String get() {
-    return WX_MP_CONFIG_STORAGE_CHOSE.get();
+    return THREAD_LOCAL.get();
   }
 
   public static void set(String label) {
-    WX_MP_CONFIG_STORAGE_CHOSE.set(label);
+    THREAD_LOCAL.set(label);
   }
 
+  /**
+   * 此方法需要用户根据自己程序代码，在适当位置手动触发调用，本SDK里无法判断调用时机
+   */
+  public static void remove() {
+    THREAD_LOCAL.remove();
+  }
 }
