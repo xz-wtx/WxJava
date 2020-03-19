868545cfc3458fb3834d865e33fc07c071a858b1
S <sshzh90@gmail.com>
2020-03-19 09:20:48 +0800

    :art: #1449 微信卡券信息增加审核状态字段

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/card/BaseInfo.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/card/BaseInfo.java
@@ -228,6 +228,18 @@ public class BaseInfo implements Serializable {
   @SerializedName("promotion_app_brand_pass")
   private String promotionAppBrandPass;
 
+
+  /**
+   *
+   * https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#2
+   * “CARD_STATUS_NOT_VERIFY”,待审核 ；
+   * “CARD_STATUS_VERIFY_FAIL”,审核失败；
+   * “CARD_STATUS_VERIFY_OK”，通过审核；
+   * “CARD_STATUS_DELETE”，卡券被商户删除；
+   * “CARD_STATUS_DISPATCH”，在公众平台投放过的卡券 ；
+   */
+  private String status;
+
   @Override
   public String toString() {
     return WxMpGsonBuilder.create().toJson(this);
