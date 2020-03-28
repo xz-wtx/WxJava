8c15b3b4b54c0c7c529adfa67a03292bc51c430f
S <sshzh90@gmail.com>
2020-03-28 17:49:53 +0800

    :art: #1475 公众号模块增加微信卡券和统计接口的一些缺失字段和枚举类
    * :sparkles: 一些优化，增加枚举类
    
    * :sparkles: 增加统计字段

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/card/Sku.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/card/Sku.java
@@ -15,10 +15,17 @@ import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
 public class Sku implements Serializable {
 
   /**
-   * 卡券库存的数量,不支持填写0，上限为100000000。
+   * 卡券库存的数量，不支持填写0，上限为100000000
    */
   @SerializedName("quantity")
   private Integer quantity = 100000000;
+
+  /**
+   * 卡券全部库存的数量，上限为100000000。
+   * https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#4
+   */
+  @SerializedName("total_quantity")
+  private Integer totalQuantity = 100000000;
 
   @Override
   public String toString() {
