07bc5a5eac07e49c70847eda029b608f31a19eb4
Binary Wang <binarywang@gmail.com>
2020-10-19 00:18:15 +0800

    :new: 微信开发平台模块增加OAuth2相关接口（网页授权、网页登录等）的实现

---
+++ weixin-java-open/src/test/java/me/chanjar/weixin/open/api/impl/WxOpenOAuth2ServiceImplTest.java
@@ -0,0 +1,38 @@
+package me.chanjar.weixin.open.api.impl;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * 单元测试.
+ *
+ * @author <a href="https://github.com/binarywang">Binary Wang</a>
+ * @date 2020-10-19
+ */
+public class WxOpenOAuth2ServiceImplTest {
+
+  @Test
+  public void testBuildAuthorizationUrl() {
+  }
+
+  @Test
+  public void testGetAccessToken() {
+  }
+
+  @Test
+  public void testTestGetAccessToken() {
+  }
+
+  @Test
+  public void testRefreshAccessToken() {
+  }
+
+  @Test
+  public void testGetUserInfo() {
+  }
+
+  @Test
+  public void testValidateAccessToken() {
+  }
+}
