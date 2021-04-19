0a75b0e902b8da3b3dd414f4d23f3b34ecde46a3
刘小狼狈 <258392906@qq.com>
2021-04-19 17:05:02 +0800

    :art: 新版微信小程序API已经不再返回 openId,unionId.删除对应字段防止开发者误用

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 public class WxMaUserInfo implements Serializable {
   private static final long serialVersionUID = 6719822331555402137L;
 
-  private String openId;
   private String nickName;
   private String gender;
   private String language;
@@ -20,7 +19,6 @@ public class WxMaUserInfo implements Serializable {
   private String province;
   private String country;
   private String avatarUrl;
-  private String unionId;
   private Watermark watermark;
 
   public static WxMaUserInfo fromJson(String json) {
