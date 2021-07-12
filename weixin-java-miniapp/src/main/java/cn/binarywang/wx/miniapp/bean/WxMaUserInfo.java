69b9ec00683dfd290af52b33189dfbd5db99ffd4
刘小狼狈 <258392906@qq.com>
2021-07-12 12:17:16 +0800

    :art: #2199 【小程序】WxMaUserInfo类补充openId和unionId字段

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
@@ -12,6 +12,7 @@ import java.io.Serializable;
 public class WxMaUserInfo implements Serializable {
   private static final long serialVersionUID = 6719822331555402137L;
 
+  private String openId;
   private String nickName;
   private String gender;
   private String language;
@@ -19,6 +20,10 @@ public class WxMaUserInfo implements Serializable {
   private String province;
   private String country;
   private String avatarUrl;
+  /**
+   * 不绑定开放平台不会返回这个字段
+   */
+  private String unionId;
   private Watermark watermark;
 
   public static WxMaUserInfo fromJson(String json) {
