1652ae2f795883ae5db8b0d677ae8a6f6410f03d
zsail <zhangsiao1314@126.com>
2021-08-04 17:33:00 +0800

    :bug: 【公众号】 短key解析接口返回结果结构修复

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/result/WxMpShortKeyResult.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/result/WxMpShortKeyResult.java
@@ -1,5 +1,6 @@
 package me.chanjar.weixin.mp.bean.result;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
 
@@ -18,14 +19,17 @@ public class WxMpShortKeyResult implements Serializable {
   /**
    * 长信息
    */
+  @SerializedName("long_data")
   protected String longData;
   /**
    * 创建的时间戳
    */
+  @SerializedName("create_time")
   protected long createTime;
   /**
    * 剩余的过期秒数
    */
+  @SerializedName("expire_seconds")
   protected long expireSeconds;
 
   public static WxMpShortKeyResult fromJson(String json) {
