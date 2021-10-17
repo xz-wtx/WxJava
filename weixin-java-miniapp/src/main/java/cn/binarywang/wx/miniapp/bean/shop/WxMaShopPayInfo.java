0c1258594ea2cbcb8e5f7b1aa2c5ad99bf0f4670
linlinjava <linlinjavaer@gmail.com>
2021-10-17 20:05:46 +0800

    修复WxMaShopPayInfo的支付方式参数

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/shop/WxMaShopPayInfo.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/shop/WxMaShopPayInfo.java
@@ -13,13 +13,13 @@ public class WxMaShopPayInfo implements Serializable {
   private static final long serialVersionUID = 687488209024968647L;
 
   /**
-   * 支付方式（目前只有"微信支付"）
+   * 支付方式（支付方式，0:微信支付，1: 货到付款，2：商家会员储蓄卡, 默认0)
    * <pre>
    * 是否必填：是
    * </pre>
    */
-  @SerializedName("pay_method")
-  private String payMethod;
+  @SerializedName("pay_method_type")
+  private Integer payMethodType;
 
   /**
    * 预支付ID
